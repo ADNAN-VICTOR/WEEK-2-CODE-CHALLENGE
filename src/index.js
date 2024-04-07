8b152a1e5c72da08ee0bb1e0752da4e069b1f6dc
ADNAN-VICTOR <162279297+ADNAN-VICTOR@users.noreply.github.com>
2024-04-07 22:06:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
+//This ensures that the JavaScript code runs only after the DOM has been fully loaded.
 $(document).ready(function() {
-    let currentFilm; // Variable to store the currently selected film
+    let currentFilm; //  this variable stores the currently selected film
 
-    // Function to fetch films data from the local server
+    // this function is used to fetch films data from the local server using an AJAX request
     function fetchFilmsData() {
         $.ajax({
             url: 'http://localhost:3000/films',
             type: 'GET',
             dataType: 'json',
             success: function(data) {
+                //calls the displayFilmTitles function to display the titles
                 displayFilmTitles(data);
             },
             error: function(error) {
@@ -19,20 +21,25 @@ $(document).ready(function() {
     // Function to display film titles
     function displayFilmTitles(films) {
         let filmsList = $('#films');
-        filmsList.empty(); // Clear previous film titles
+        filmsList.empty(); // Clears the previous film titles
 
-        // Loop through each film and append title as an anchor tag to filmsList
+        // it Loops through each film 
         films.forEach(function(film) {
+            // displays film titles as clickable links.
             let filmLink = $('<a>').addClass('film item').attr('href', '#').text(film.title);
             filmLink.click(function() {
+                //When a film link is clicked, it sets the currentFilm variable 
+                //and calls the updateFilmDetails function to display the details of the selected film.
                 currentFilm = film;
                 updateFilmDetails();
             });
+                //it then appends the change
             filmsList.append(filmLink);
         });
     }
 
-    // Function to update film details when a film is selected
+    // This function updates the film details section
+    //with information about the currently selected film after it has been clicked.
     function updateFilmDetails() {
         $('#title').text(currentFilm.title);
         $('.meta').text(currentFilm.runtime + ' minutes');
@@ -56,6 +63,7 @@ $(document).ready(function() {
         }
     });
 
-    // Fetch films data when the document is ready
+   //function is called when the document is ready to fetch the film data 
+   //and populate the page with film titles.
     fetchFilmsData();
 });
